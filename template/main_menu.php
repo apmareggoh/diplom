7388e846e4722f6fab7c0b7b0ec240a8b62d74c9
apmareggoh <apmareggoh@bk.ru>
2014-03-10 16:11:51 +0400

    шаблн версия 0.2

--- template/main_menu.php
+++ template/main_menu.php
@@ -4,4 +4,8 @@
  * User: йцукенг
  * Date: 10.03.14
  * Time: 11:55
- */ 
+ */
+echo '<li><a href="/" ' . ( $url[1] ? '' : 'class="active"' ) . '>Главная</a></li>
+          <li><a href="services.html"' . ( $url[1] == 'tren' ? 'class="active"' : '' ) . '>Тренажер</a></li>
+          <li><a href="about.html"' . ( $url[1] == 'test' ? 'class="active"' : '' ) . '>Тест</a></li>
+          <li><a href="contact.html"' . ( $url[1] == 'about' ? 'class="active"' : '' ) . '>Контакты</a></li>';
---
+++ template/template.php
@@ -0,0 +1,63 @@
+<? echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>GreenyBox</title>
+<meta http-equiv="content-type" content="text/html;" charset=utf-8" />
+<link href="/template/style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="/template/js/cufon-yui.js"></script>
+<script type="text/javascript" src="/template/js/arial.js"></script>
+<script type="text/javascript" src="/template/js/cuf_run.js"></script>
+</head>
+<body>
+<div class="main">
+  <div class="header">
+    <div class="header_resize">
+      <div class="logo">
+        <h1><a href="/"><span>Бинарные</span>Деревья <small>БГТУ 2014 </small></a></h1>
+      </div>
+      <div class="menu">
+        <ul>';
+      require('template/main_menu.php');
+        echo '</ul>
+      </div>
+      <div class="clr"></div>
+    </div>
+    <div class="headert_text_resize">
+      <div class="headert_text">
+        <h2>Дипломная работа</h2>
+        <p>Выполнена весной 2014 года на базе Брянского Государственного Технического Университета</p>
+      </div>
+      <img src="/template/images/img_main.jpg" alt="" width="455" height="273" />
+      <div class="clr"></div>
+    </div>
+  </div>
+  <div class="body">
+    <div class="body_resize">
+      <div class="left">
+        <div class="resize_bg">';
+
+if ( $title ) {
+  echo '<h2>' . $title . '</h2>';
+}
+if ( $image ) {
+  echo '<img alt="" width="455" height="273" scr = "' . $image . '"/>';
+}
+if ( $text ) {
+  echo $text;
+}
+echo '
+        </div>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
+  <div class="footer">
+    <div class="footer_resize">
+      <p class="lf">Copyright &copy; <a href="#">BSTU</a>. All Rights Reserved</p>
+      <div class="clr"></div>
+    </div>
+    <div class="clr"></div>
+  </div>
+</div>
+</body>
+</html>';
